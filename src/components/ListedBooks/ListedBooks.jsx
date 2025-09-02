05444e0453f51c36234d988d68d4ed8537bede48
Imamul Kadir <imamulkadiroyan@gmail.com>
2025-09-02 07:16:02 +0600

    updated sort options

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -10,6 +10,7 @@ const ListedBooks = () => {
   const [readList, setReadList] = useState([]);
   const allBooks = useLoaderData();
   const [wishlistList, setWishlistList] = useState([]);
+  const [sort, setSort] = useState("");
 
   useEffect(() => {
     const storedReadList = getStoredReadList();
@@ -27,29 +28,70 @@ const ListedBooks = () => {
     setWishlistList(wishlistBookList);
   }, []);
 
+  const handleSort = (sortType) => {
+    setSort(sortType);
+
+    if (sortType === "Ratings") {
+      const sortedReadList = [...readList].sort((a, b) => b.rating - a.rating);
+      setReadList(sortedReadList);
+    } else {
+      const sortedReadList = [...readList].sort(
+        (a, b) => b.totalPages - a.totalPages,
+      );
+      setReadList(sortedReadList);
+    }
+    // const sorteReadList = [...readList].sort((a, b) => {
+    //   if (sortType === "Ratings") {
+    //     return b.rating - a.rating;
+    //   } else {
+    //     return b.totalPages - a.totalPages;
+    //   }
+    // });
+    // setReadList(sorteReadList);
+  };
+
   return (
-    <Tabs className="py-10">
-      <TabList>
-        <Tab>Read List ({readList.length})</Tab>
-        <Tab>Wishlist ({wishlistList.length})</Tab>
-      </TabList>
-
-      <TabPanel>
-        <div className="grid grid-cols-1 space-y-4">
-          {readList.map((book) => (
-            <ReadList key={book.bookId} book={book} />
-          ))}
+    <div className="flex flex-col items-center justify-center">
+      <div className="dropdown dropdown-center my-10">
+        <div tabIndex={0} role="button" className="btn m-1">
+          {sort ? `Sort by: ${sort} ⬇️` : "Sort by ⬇️"}
         </div>
-      </TabPanel>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          onClick={() => document.activeElement.blur()}
+        >
+          <li onClick={() => handleSort("Ratings")}>
+            <a>Ratings</a>
+          </li>
+          <li onClick={() => handleSort("Number of Pages")}>
+            <a>Number of Pages</a>
+          </li>
+        </ul>
+      </div>
+      <Tabs className="py-10 lg:w-full">
+        <TabList>
+          <Tab>Read List ({readList.length})</Tab>
+          <Tab>Wishlist ({wishlistList.length})</Tab>
+        </TabList>
 
-      <TabPanel>
-        <div className="grid grid-cols-1 space-y-4">
-          {wishlistList.map((book) => (
-            <WishList key={book.bookId} book={book} />
-          ))}
-        </div>
-      </TabPanel>
-    </Tabs>
+        <TabPanel>
+          <div className="grid grid-cols-1 space-y-4">
+            {readList.map((book) => (
+              <ReadList key={book.bookId} book={book} />
+            ))}
+          </div>
+        </TabPanel>
+
+        <TabPanel>
+          <div className="grid grid-cols-1 space-y-4">
+            {wishlistList.map((book) => (
+              <WishList key={book.bookId} book={book} />
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
   );
 };
 
